033035ccc382af06232869abed381d0a75575b90
BumCyrilYoah <yoahcyril8@gmail.com>
2024-11-15 11:47:55 +0100

    formula

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numberOne;
+    int numberTwo;
+    int numberThree;
+    int sum;
+    printf("enter numberOne\n");
+    scanf("%d",&numberOne);
+
+        printf("enter numberTwo\n");
+        scanf("%d",&numberTwo);
+            printf("enter numberThree\n");
+
+    return 0;
+}
